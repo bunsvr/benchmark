8641cd9de9245fd15adcefa524c0ee46d8b072c2
aquapi <aquaplmc@gmail.com>
2023-01-24 22:50:14 +0700

    Update

--- src/Colston/index.ts
+++
@@ -1,9 +0,0 @@
-import Colston from "colstonjs";
-
-new Colston()
-    .get("/", c => c.text("Hi"))
-    .get("/id/:id", c => 
-        c.text(c.request.params.id))
-    .post("/json", async c => 
-        c.json(await c.request.json()))
-    .start(3000);
