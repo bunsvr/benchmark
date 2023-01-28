7c48a3c21109346fc356d0612d90a96f398b11bb
Revenity <87530663+aquapi@users.noreply.github.com>
2023-01-28 22:07:12 +0700

    Update package name

---
+++ src/Stric/index.ts
@@ -0,0 +1,12 @@
+import { Router } from "@bunsvr/router";
+
+new Router()
+    .static("GET", "/", 
+        () => new Response("Hi"))
+    .static("POST", "/json",
+        async req => new Response(
+            JSON.stringify(await req.json())
+        ))
+    .dynamic("GET", "/id/:id", 
+        req => new Response(req.params[1]))
+    .serve();
