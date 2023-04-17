08d965487fefc618ae0f52125e67ad0c921b3255
Revenity <aquaplmc@gmail.com>
2023-04-17 16:21:03 +0000

    Update deps and results

--- src/Bagel/index.ts
+++
@@ -1,9 +0,0 @@
-import { Bagel } from "@kakengloh/bagel";
-
-new Bagel()
-    .get("/", async (_, res) => res.send("Hi"))
-    .get("/id/:id", async (req, res) => 
-        res.send(req.params.id))
-    .post("/json", async (req, res) => 
-        res.json(req.body))
-    .listen(3000);
