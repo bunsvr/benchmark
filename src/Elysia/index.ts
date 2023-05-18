3b7054b103d7be53ca78d3f3957a93399ff1ec25
saltyaom <saltyaom@gmail.com>
2023-05-18 21:33:28 +0700

    feat(elysia): add explicity parsing

--- src/Elysia/index.ts
+++ src/Elysia/index.ts
@@ -1,7 +1,9 @@
-import Elysia from "elysia";
+import Elysia from 'elysia'
 
 new Elysia()
-    .get("/", () => "Hi")
-    .get("/id/:id", c => c.params.id)
-    .post("/json", c => c.body)
-    .listen(3000);
+    .get('/', () => 'Hi')
+    .get('/id/:id', (c) => c.params.id)
+    .post('/json', (c) => c.body, {
+        type: 'json'
+    })
+    .listen(3000)
