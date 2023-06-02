6b153a86a73082386c4f3dbb2f52cf83770fe66b
Revenity <87530663+aquapi@users.noreply.github.com>
2023-06-02 10:19:40 +0700

    Perf still the same, just for more convenient

--- src/Stric/index.ts
+++ src/Stric/index.ts
@@ -6,8 +6,6 @@ const opts = {
 
 export default new Router() 
     .get('/', () => new Response('Hi'))
-    .post('/json', async req => new Response(
-        JSON.stringify(await req.json()), opts
-    ))
-    .get('/id/:id', req => new Response(req.params.id))
+    .post('/json', async req => new Response(JSON.stringify(await req.json()), opts))
+    .get('/id/:id', ({ params: { id } }) => new Response(id))
     .use(404);
