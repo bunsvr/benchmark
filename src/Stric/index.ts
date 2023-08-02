48fab9fb281a8265e2feea23f90cdd83c0567ca7
aquapi <aquaplmc@gmail.com>
2023-08-02 23:17:05 +0700

    Update

--- src/Stric/index.ts
+++ src/Stric/index.ts
@@ -3,9 +3,7 @@ import { qs } from '@stricjs/utils';
 
 const toRes = Response.json, parse = qs.searchKey('name');
 
-export default new Router({ 
-    base: 'http://localhost:3000'
-})
+export default new Router({ base: 'http://localhost:3000' })
     .get('/', macro(() => new Response('Hi')))
     .post('/json', req => req.json().then(toRes))
     .get('/id/:id', req => new Response(
