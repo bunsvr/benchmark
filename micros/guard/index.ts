08b54254bd9263262e874b2eed31e809994bf87f
aquapi <aquaplmc@gmail.com>
2023-08-15 15:30:11 +0700

    Deeply nested object

--- micros/guard/index.ts
+++ micros/guard/index.ts
@@ -3,41 +3,28 @@ import { guard } from '@stricjs/utils';
 import { Type } from '@sinclair/typebox';
 import { Value } from '@sinclair/typebox/value';
 
-// Simple validation
-group('Simple', () => {
-    const stric = guard.create({
-        name: 'str',
-        age: 'num'
-    }), typebox = Type.Object({
-        name: Type.String(),
-        age: Type.Number()
-    });
-
-    const o = { name: 'John', age: 50 };
-
-    bench('Stric guard', () => {
-        stric(o);
-    });
-
-    bench('Typebox', () => {
-        Value.Check(typebox, o);
-    });
-});
-
 group('Nested', () => {
     const stric = guard.create({
         name: 'str',
         age: 'num',
         hobby: {
             name: 'str',
-            since: 'num'
+            since: 'num',
+            more: {
+                avgScore: 'num',
+                weight: 'num'
+            }
         }
     }), typebox = Type.Object({
         name: Type.String(),
         age: Type.Number(),
         hobby: Type.Object({
             name: Type.String(),
-            since: Type.Number()
+            since: Type.Number(),
+            more: Type.Object({
+                avgScore: Type.Number(),
+                weight: Type.Number()
+            })
         })
     });
 
@@ -45,17 +32,21 @@ group('Nested', () => {
         name: 'John', age: 50, 
         hobbies: {
             name: 'badminton',
-            since: 15
+            since: 15,
+            more: {
+                avgScore: 90,
+                weight: 67
+            }
         } 
     };
-
-    bench('Stric guard', () => {
-        stric(o);
-    });
 
     bench('Typebox', () => {
         Value.Check(typebox, o);
     });
+
+    bench('Stric guard', () => {
+        stric(o);
+    }); 
 });
 
-await run();
+run();
