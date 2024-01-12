5a5e97b23ecc9ec88305d69baa0ef4ceee33cab5
aquapi <aquaplmc@gmail.com>
2024-01-12 16:54:07 +0700

    Accurate tests

--- micros/guard/index.ts
+++ micros/guard/index.ts
@@ -5,16 +5,6 @@ import vld from './src/vld';
 
 import * as obj from './src/utils/obj';
 
-// Inspect validators
-console.log('Typebox:', typebox.toString());
-console.log('VLD:', vld.toString());
-
-// Run these functions a few times to force JIT optimizations
-for (i = 1e9; i > 0; --i) {
-    typebox(obj.valid);
-    vld(obj.valid);
-}
-
 // Avoid JIT bias
 for (var i = 50; i > 0; --i)
     bench(i.toString(), () => { });
